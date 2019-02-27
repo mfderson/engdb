e49fee6187bcf7189d5617435278e8ab6562fce4
Anderson Freitas <mfderson@gmail.com>
2019-02-27 08:41:05 -0300

    Projeto criado

---
+++ src/main/java/com/anderson/engdb/EngdbApplication.java
@@ -0,0 +1,13 @@
+package com.anderson.engdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anderson/engdb/EngdbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.anderson.engdb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EngdbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
